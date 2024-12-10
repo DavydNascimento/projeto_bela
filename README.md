2a662218c1aee5a7396d4b2a18effe8276ff8336
Davyd Nascimento <davyd.seth19@gmail.com>
2024-12-10 12:27:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto_bela
+ Site da Bela
---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('bookingForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Agendamento realizado com sucesso!');
+});
